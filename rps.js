55e17d06d9749bf91cf5b469e363b9fe537d83ca
mirazrq <rqmiraz074@gmail.com>
2021-03-22 18:55:10 +0600

    store score and display.

--- rps.js
+++ rps.js
@@ -4,26 +4,35 @@ function computerPlay(){
     return options[n];
 }
 
+let pla=0;
+let com=0;
+
 function playRound(playerSelection,computerSelection){
     const pl=playerSelection.toLowerCase()
     const cp=computerSelection.toLowerCase()
 
     if(pl==="rock" && cp ==="paper"){
+        com++;
         return "You Lose! Paper beats Rock!"
     }
     else if(pl==="rock" && cp==="scissors"){
+        pla++;
         return "You win! Rock beats Scissors!"
     }
     else if(pl==="paper" && cp=="rock"){
+        pla++;
         return "You win! Paper beats Rock!"
     }
     else if(pl==="paper" && cp ==="scissors"){
+        com++;
         return "You Lose! Scissors beats Paper!"
     }
     else if(pl==="scissors" && cp === "paper"){
+        pla++;
         return "You win! Scissors beats Paper!"
     }
     else if(pl==="scissors" && cp==="rock"){
+        com++;
         return "You lose! Rock beats Scissors"
     }
     else if(pl===cp){
@@ -38,7 +47,8 @@ function playRound(playerSelection,computerSelection){
 function game(){
       for(let i =0; i < 5;i++){
           gameplay();
-      }  
+      }
+      console.log("Player:"+pla+" "+"Computer:"+com);
 }
 
 
